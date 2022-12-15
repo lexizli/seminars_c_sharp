b596eef8cf71edb454e4307e4a87a6c1bb80326b
Lexizli <pho_lex-r@yahoo.com>
2022-12-15 21:08:53 +0300

    is a square of b?

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Console.Write("Input some integer, please — ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+double b = 7 / 13;
+
+Console.WriteLine("Inputed number is " + number + " Hello, World! " + b);
+*/
+// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.Write("Input some integer, please — ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input one more integer, please — ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if ( number1 / number2 == number2 ) Console.WriteLine($"Inputed number {number1} is square of {number2}");
+else Console.WriteLine($"Sorry, inputed number {number1} is not square of {number2}");
