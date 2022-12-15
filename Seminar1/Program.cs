a44f29408bd0a0e6d538c5fedc1ced02115d4ecd
Lexizli <pho_lex-r@yahoo.com>
2022-12-15 21:41:14 +0300

    more and more...

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -7,8 +7,11 @@ double b = 7 / 13;
 
 Console.WriteLine("Inputed number is " + number + " Hello, World! " + b);
 */
-// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
-
+/*
+Задача 1. 
+Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+*/
+/*
 Console.Write("Input some integer, please — ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input one more integer, please — ");
@@ -16,3 +19,21 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 
 if ( number1 / number2 == number2 ) Console.WriteLine($"Inputed number {number1} is square of {number2}");
 else Console.WriteLine($"Sorry, inputed number {number1} is not square of {number2}");
+*/
+
+/*
+Задача 2.
+Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+*/
+/*
+Console.Write("Input some integer, please — ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 <0) number1 = -number1;
+int key = -number1;
+while (key <= number1) 
+{
+    Console.WriteLine(key);
+    key++;
+
+}
+*/
