e102a07d66d4142172fe15c45cbad473597d7017
Lexizli <pho_lex-r@yahoo.com>
2023-02-03 13:00:36 +0300

    Homework9 is so simple and ready

---
+++ Homework9/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+void ShowToOne(int number)
+{
+    Console.Write(number + " ");
+    if(number < 1) ShowToOne(number + 1);
+    if(number > 1) ShowToOne(number - 1);
+}
+
+
+ShowToOne(22);
+Console.WriteLine();
+
+ShowToOne(-22);
+Console.WriteLine();
+*/
+
+/*
+int SumFromTo(int num1, int num2)
+{
+    int sum = 0;
+    if(num1 > num2) return sum = sum + num1 + SumFromTo(num1 - 1, num2);
+    if(num1 < num2) return sum = sum + num1 + SumFromTo(num1 + 1, num2);
+    return sum + num1;
+}
+
+Console.WriteLine(SumFromTo(15,1));
+Console.WriteLine(SumFromTo(1,15));
+Console.WriteLine(SumFromTo(4,8));
+Console.WriteLine(SumFromTo(8,4));
+*/
+
+int Ackermasha(int num1, int num2)
+{
+    if(num1 > 0)
+    {
+        if(num2 > 0) return Ackermasha(num1 - 1, Ackermasha(num1, num2 - 1));
+        else return Ackermasha(num1 - 1, 1);
+    }
+    else return num2 + 1;
+}
+
+Console.WriteLine(Ackermasha(2, 3));
+Console.WriteLine(Ackermasha(3, 2));
+Console.WriteLine(Ackermasha(3, 4));
+Console.WriteLine(Ackermasha(3, 5));
+
---
+++ Seminar9/Program.cs
@@ -0,0 +1,60 @@
+﻿
+/*
+void ShowNums(int num)
+{
+    Console.Write(num + " ");
+    if(num > 1) ShowNums(num - 1);
+    Console.Write(num + " ");
+}
+
+ShowNums(25);
+*/
+/*
+
+int SumOfDigits(int num)
+{
+    if(num > 0) return SumOfDigits(num/10) + num % 10;
+    else return 0;
+
+}
+
+Console.WriteLine(SumOfDigits(64738291));
+*/
+/*
+Задача 1
+Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Задача 2
+Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
+*/
+
+/*
+void ShowSerialNums(int num1, int num2)
+{
+    // num1 = 10 num2 = 5
+   if (num1 > num2)
+    {
+        Console.Write(num1 + " ");
+        ShowSerialNums(num1 - 1, num2);
+    }
+        
+    else if (num1 < num2)
+    {
+        ShowSerialNums(num2 - 1, num1);
+        Console.Write(num2 + " ");
+    }
+    else Console.Write(num2 + " ");
+
+}
+
+ShowSerialNums(2, 10);
+*/
+
+double Stepen(double num1, int num2)
+{
+    if(num2 > 0) return num1 * Stepen(num1, num2 - 1);
+    if(num2 < 0) return 1/num1 * Stepen(num1, num2 + 1);
+    else return 1;
+}
+
+Console.WriteLine(Stepen(2, -4));
